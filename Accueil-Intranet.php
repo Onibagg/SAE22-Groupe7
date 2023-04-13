c97167bbdebb909368630f02f3398163f7421ebe
Gabin Demé <129855477+Onibagg@users.noreply.github.com>
2023-04-13 08:22:47 +0200

    navbar
    First test of Vertical Nav

--- Accueil-Intranet.php
+++ Accueil-Intranet.php
@@ -2,4 +2,15 @@
 include("fonctions.php");
 setup();
 ?>
-<h1>Bienvenu dans l'Intranet</h1>
+<div class="row">
+    <div class="col-sm-1 bg-dark text-center"><?php intranet_navbar(); ?></div>
+    <div class="col-sm-11 bg-primary">
+        <h1>Bienvenu dans l'Intranet</h1>
+    </div>
+</div>
+
+
+
+
+
+
