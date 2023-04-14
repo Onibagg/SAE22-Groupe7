98b96440713396ec4e1706ecb5c7bc3ef0339f26
Onibagg <129855477+Onibagg@users.noreply.github.com>
2023-04-14 07:57:14 +0200

    work on "Portail de connexion"
    + creation of the table
    + del user

--- Portail-de-connexion.php
+++ Portail-de-connexion.php
@@ -62,5 +62,20 @@ intranet_navbar();
 
     </div>
 </div>
+
+<br><br>
+
+<div class="container mt-2">
+    <div class="mt-2 p-4 bg-dark text-white rounded">
+        <p class="display-6 "><b>Vos utilisateurs :</b></p>
+    </div>
+    <div class="mt-2 p-4 bg-light text-dark rounded">
+        <?php
+        gestionUtilisateurs();
+        ?>
+    </div>
+</div>
+<br>
+<br>
 </div>
 </div>
