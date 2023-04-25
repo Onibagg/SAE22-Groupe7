5b3e6f84c7782f299267eea88f1ebeb1e028b976
Omega7222 <129857024+Omega7222@users.noreply.github.com>
2023-04-25 10:46:19 +0200

    Modification des pages juridiques

--- Politique_de_confidentialité.php
+++ Politique_de_confidentialité.php
@@ -26,7 +26,7 @@ setup();
                 <div class="col-sm-10 text-dark">
                     </br>
                     </br>
-                    <h1>Politique sur le respect de la vie privée</h1>
+                    <h1>Politique de confidentialité</h1>
                     </br>
                     </br>
                     <p>Ce document (« Politique de confidentialité ») explique les règles de confidentialité applicables aux données à caractère personnel et autres informations collectées ou soumises lorsque vous accédez, installez ou utilisez les Services, quel que soit l'appareil (ordinateur, téléphone mobile, tablette, etc.) que vous utilisez.</p>
@@ -53,16 +53,16 @@ setup();
                     <li>Informations d'ordre économique et financier (revenus, situation financière, situation fiscale...)</li>
                     </ul>
                     </br>
-                    </br>
+                    
                     <h3>3 - Partage de vos données à caractère personnel</h3>
                     </br>
                     
                     <p>Nous ne partageons pas vos données à caractère personnel avec des tiers, sauf dans les conditions décrites dans la présente Politique de confidentialité.</p>
-                    </br>
+                    
                     
                     <p><strong>Prestataires de services : </strong>Nous faisons appel à des prestataires de services tiers pour nous aider dans diverses opérations, telles que le traitement des paiements, l’automatisation des e-mails, le diagnostic des sites Web et des applications, l’analyse et autres. Par conséquent, certains de ces fournisseurs de services peuvent traiter des données à caractère personnel.</p>
                     </br>
-                    </br>
+                    
                     <h3>4 - Choix liés à vos données à caractère personnel</h3>
                     <p>Il existe différentes lois sur la protection des données qui vous confèrent des droits en matière de confidentialité en tant que personne concernée. Sous réserve des lois applicables en matière de protection des données, vous pouvez avoir les droits suivants :</p>
                     <ul>
@@ -90,14 +90,37 @@ setup();
                     </ol>
                     
                     <p><strong>Désabonnement</strong> : Si vous ne souhaitez plus recevoir de communications de PrivateVPN, vous pouvez vous désinscrire à tout moment en cliquant sur le lien « se désabonner » au bas de chaque e-mail ou en nous contactant à l'adresse privacy@PrivateVPNaccount.com.</p>
-                    
-                    <h3>Sécurité des données</h3>
+                    </br>       
+                    <h3>5 - Sécurité des données</h3>
                     <p>Nous avons mis en place des mesures physiques, techniques et organisationnelles pour protéger les données personnelles contre toute forme de traitement illégal ou d'accès non autorisé.</p>
                     <p><strong>Mesures physiques :</strong> Accès contrôlé aux installations, stockage des appareils dans des endroits fermés à clé, et politiques de bureau bien rangé.</p>
                     <p><strong>Mesures techniques :</strong> Système de défense en couches avec pare-feu, protection contre les logiciels malveillants, détection et prévention des intrusions, régulièrement mis à jour avec des scans de vulnérabilité. Les serveurs sont renforcés et tous les postes de travail sont gérés depuis un outil de gestion centralisé. Les données sont chiffrées et les protocoles de chiffrement sont conformes aux pratiques de sécurité les plus récentes.</p>
                     <p><strong>Mesures organisationnelles :</strong> Politiques de sécurité de l'information et de traitement des données conformes aux meilleures pratiques. Audits externes pour prouver la conformité aux normes. Culture de développement constant de la sécurité et de la sensibilisation à la protection des données parmi les employés, avec des formations régulières et continues. Nous analysons les menaces et la surface d'attaque et mettons constamment à jour nos mesures de sécurité. L'accès aux bases de données contenant des données à caractère personnel est accordé sur la base du besoin de savoir.</p>
-                    <p>Si nous détectons quelque chose de suspect, nous vous informerons immédiatement et vous guider
-
+                    <p>Si nous détectons quelque chose de suspect, nous vous informerons immédiatement et vous guider</p>
+</br>
+                    <h3>6 - Conservation et suppression des données</h3>
+                    <p>privateVPN conservera vos données à caractère personnel aussi longtemps que nécessaire pour vous fournir les Services, ou aussi longtemps que nous avons un autre motif légitime de le faire, mais pas plus longtemps que ce qui est permis ou exigé par la loi. Certains des termes de conservation des données plus spécifiques sont fournis ci-dessous :</p>
+                    <ul>
+                    <li>Les informations de facturation du client et les détails du paiement sont conservés par privateVPN pendant 10 ans à compter de la dernière transaction de paiement.</li>
+                    <li>privateVPN utilisera votre e-mail pour la communication marketing pendant 1 an après la fin de votre Abonnement.</li>
+                    </ul>
+                    <p>Lorsque nous n'avons plus de motif légal pour conserver vos données à caractère personnel, elles seront soit éliminées de manière sécurisée, soit dépersonnalisées par des moyens d'anonymisation appropriés.</p>
+                    </br>
+                    <h3>7 - Géolocalisation</h3>
+                    
+                    <p><strong>Géolocalisation à des fins de fourniture du service</strong>Nous collectons et traitons vos données de géolocalisation afin de vous fournir nos services. Nous pouvons être amenés à faire usage des données personnelles dans le but de déterminer votre position géographique en temps réel. Conformément à votre droit d'opposition prévu par la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés, vous avez la possibilité, à tout moment, de désactiver les fonctions relatives à la géolocalisation.</p>
+                    
+                    <p><strong>Géolocalisation à des fins de croisement</strong>Nous collectons et traitons vos données de géolocalisation afin de permettre à nos services d'identifier les points de croisement dans le temps et dans l'espace avec d'autres Utilisateurs du service afin de vous présenter le profil des Utilisateurs croisés. Conformément à votre droit d'opposition prévu par la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés, vous avez la possibilité, à tout moment, de désactiver les fonctions relatives à la géolocalisation. Vous reconnaissez alors que le service ne sera plus en mesure de vous présenter de profil des autres Utilisateurs.</p>
+                    <p><strong>Géolocalisation avec mise à disposition des partenaires pour référencement et agrégation</strong>Nous pouvons collecter et traiter vos données de géolocalisation avec nos partenaires. Nous nous engageons à anonymiser les données utilisées. Conformément à votre droit d'opposition prévu par la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés, vous avez la possibilité, à tout moment, de désactiver les fonctions relatives à la géolocalisation.</p>
+                    </br>
+                    <h3>8 - Cookies</h3>
+                    
+                    <p><strong>Durée de conservation des cookies</strong>Conformément aux recommandations de la CNIL, la durée maximale de conservation des cookies est de 13 mois au maximum après leur premier dépôt dans le terminal de l'Utilisateur, tout comme la durée de la validité du consentement de l’Utilisateur à l’utilisation de ces cookies. La durée de vie des cookies n’est pas prolongée à chaque visite. Le consentement de l’Utilisateur devra donc être renouvelé à l'issue de ce délai.</p>
+                    
+                    <p><strong>Finalité cookies</strong>Les cookies peuvent être utilisés pour des fins statistiques notamment pour optimiser les services rendus à l'Utilisateur, à partir du traitement des informations concernant la fréquence d'accès, la personnalisation des pages ainsi que les opérations réalisées et les informations consultées.</p>
+                    <p>Vous êtes informé que l'Éditeur est susceptible de déposer des cookies sur votre terminal. Le cookie enregistre des informations relatives à la navigation sur le service (les pages que vous avez consultées, la date et l'heure de la consultation...) que nous pourrons lire lors de vos visites ultérieures.</p>
+                    
+                    <p><strong>Droit de l'Utilisateur de refuser les cookies</strong>Vous reconnaissez avoir été informé que l'Éditeur peut avoir recours à des cookies. Si vous ne souhaitez pas que des cookies soient utilisés sur votre terminal, la plupart des navigateurs vous permettent de désactiver les cookies en passant par les options de réglage.</p>
                 </div>
                 <div class="col-sm-1 text-dark"></div>
             </div>
