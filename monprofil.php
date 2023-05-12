cbfc7ca560dcb787d29d6265c7d630bfb772b367
Gabin Demé <129855477+Onibagg@users.noreply.github.com>
2023-05-12 11:16:13 +0200

    Additions on new profil changes

--- monprofil.php
+++ monprofil.php
@@ -18,46 +18,71 @@ intranet_navbar();
                     <input type="file" class="form-control-file" id="photo" name="photo">
                 </div>
         </div>
-        <div class="col-4 card">
-            <div class="form-floating mb-3 mt-3">
-                <input type="text" class="form-control" id="nom" placeholder="Nom" name="nom">
-                <label for="nom">Nom</label>
-            </div>
-            <div class="form-floating mb-3 mt-3">
-                <input type="text" class="form-control" id="prenom" placeholder="Prénom" name="prenom">
-                <label for="prenom">Prénom</label>
-            </div>
-            <div class="form-floating mb-3 mt-3">
-                <input type="text" class="form-control" id="user" placeholder="Nom d'utilisateur" name="user">
-                <label for="user">Nom d'utilisateur</label>
-            </div>
-            <div class="form-floating mb-3 mt-3">
-                <input type="text" class="form-control" id="email" placeholder="Email" name="email">
-                <label for="email">Email</label>
-            </div>
-            <div class="form-floating mb-2 mt-5">
-                <input type="password" class="form-control" id="mdp" placeholder="Mot de passe" name="mdp">
-                <label for="mdp">Nouveau Mot de passe</label>
-            </div>
-            <div class="form-floating mb-3 mt-2">
-                <input type="password" class="form-control" id="mdp" placeholder="Mot de passe" name="mdp">
-                <label for="mdp">Confirmation</label>
-            </div>
-        </div>
-        <div class="col-2">
-        </div>
-        <div class="mt-5 mb-5 text-center">
-            <button name="new-profile" type="submit" class="btn btn-outline-dark">Enregistrer les modifications</button>
-            </form>
-        </div>
+        <?php
+        $user = $_SESSION['user'];
+        $data = file_decod('Data/login-mdp.json');
 
+        if (isset($data[$user])) {
+            $prenom = $data[$user]['prenom'];
+            $nom = $data[$user]['nom'];
+            $mdp = $data[$user]['mdp'];
+            $email = $data[$user]['email'];
+            $groupe = $data[$user]['groupe'];
+            $poste = $data[$user]['poste'];
 
-
-
+        ?>
+            <div class="col-4 card">
+                <div class="input-group mb-3 mt-3">
+                    <span class="input-group-text">Prénom</span>
+                    <div class="form-floating">
+                        <input type="text" class="form-control" id="prenom" placeholder="<?php echo $prenom; ?>" name="prenom">
+                        <label for="prenom"><?php echo $prenom; ?></label>
+                    </div>
+                </div>
+                <div class="input-group mb-3 mt-3">
+                    <span class="input-group-text">Prénom</span>
+                    <div class="form-floating">
+                        <input type="text" class="form-control" id="nom" placeholder="<?php echo $nom; ?>" name="nom">
+                        <label for="nom"><?php echo $nom; ?></label>
+                    </div>
+                </div>
+                <div class="input-group mb-3 mt-3">
+                    <span class="input-group-text">User</span>
+                    <div class="form-floating">
+                        <input type="text" class="form-control" id="user" placeholder="<?php echo $user; ?>" name="user" disabled>
+                        <label for="user"><?php echo $user; ?></label>
+                    </div>
+                </div>
+                <div class="input-group mb-3 mt-3">
+                    <span class="input-group-text">E-mail</span>
+                    <div class="form-floating">
+                        <input type="text" class="form-control" id="email" placeholder="<?php echo $email; ?>" name="email">
+                        <label for="email"><?php echo $email; ?></label>
+                    </div>
+                </div>
+                <div class="form-floating mb-2 mt-5">
+                    <input type="password" class="form-control" id="nouveauMdp" placeholder="Mot de passe" name="nouveauMdp">
+                    <label for="nouveauMdp">Nouveau Mot de passe</label>
+                </div>
+                <div class="form-floating mb-3 mt-2">
+                    <input type="password" class="form-control" id="confirmationMdp" placeholder="Mot de passe" name="confirmationMdp">
+                    <label for="confirmationMdp">Confirmation</label>
+                </div>
+            </div>
+            <div class="col-2">
+            </div>
+            <div class="mt-5 mb-5 text-center">
+                <button name="new-profile" type="submit" class="btn btn-outline-success">Enregistrer les modifications</button>
+                </form>
+            </div>
+        <?php
+        }
+        ?>
     </div>
 </div>
 
 <?php
+//  $nouveauMdp === $confirmationMdp
 if (isset($_POST['new-profile'])) {
     if (isset($_FILES['photo']) && $_FILES['photo']['error'] === 0) {
         $targetDir = 'Images/Employés/';
@@ -71,5 +96,35 @@ if (isset($_POST['new-profile'])) {
         $newFile = $targetDir . $_SESSION['user'] . '.jpg';
         move_uploaded_file($tmpFile, $newFile);
     }
+
+    $prenom = $_POST['prenom'];
+    $nom = $_POST['nom'];
+    $user = $_POST['user'];
+    $email = $_POST['email'];
+    $nouveauMdp = $_POST['nouveauMdp'];
+    $confirmationMdp = $_POST['confirmationMdp'];
+
+    // Valider et mettre à jour les informations de l'utilisateur
+    $data = json_decode(file_get_contents('Data/login-mdp.json'), true);
+    if (isset($data[$user])) {
+        $data[$user]['prenom'] = $prenom;
+        $data[$user]['nom'] = $nom;
+        $data[$user]['email'] = $email;
+
+        // Vérifier si un nouveau mot de passe a été saisi et le mettre à jour
+        if (!empty($nouveauMdp)) {
+            $data[$user]['mdp'] = $nouveauMdp;
+        }
+
+        // Sauvegarder les modifications dans le fichier JSON
+        file_put_contents('Data/login-mdp.json', json_encode($data));
+
+        // Rediriger vers une page de confirmation ou effectuer d'autres actions
+        header("Location: confirmation.php");
+        exit();
+    }
 }
+
+
+
 ?>
