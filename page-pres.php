82cf77c65c4ea60d86d96209d76d558784957efb
Emmanuel35400 <emmanuel.chapin@etudiant.univ-rennes1.fr>
2023-05-19 15:09:58 +0200

    apropos v1

--- page-pres.php
+++ page-pres.php
@@ -3,397 +3,175 @@ include("fonctions.php");
 setup();
 page_header();
 ?>
-<p class="display-4 text-center">Qui sommes-nous ?</p>
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
 
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
-
-<div class="content">
-  <figure class="org-chart cf">
-    <ul class="administration">
-      <li>    				
-        <ul class="director">
-          <li>
-            <a href="#"><span>Director</span></a>
-            <ul class="subdirector">
-              <li><a href="#"><span>Assistante Director</span></a></li>
-            </ul>
-            <ul class="departments cf">								
-              <li><a href="#"><span>Administration</span></a></li>
-              
-              <li class="department dep-a">
-                <a href="#"><span>Department A</span></a>
-                <ul class="sections">
-                  <li class="section"><a href="#"><span>Section A1</span></a></li>
-                  <li class="section"><a href="#"><span>Section A2</span></a></li>
-                  <li class="section"><a href="#"><span>Section A3</span></a></li>
-                  <li class="section"><a href="#"><span>Section A4</span></a></li>
-                  <li class="section"><a href="#"><span>Section A5</span></a></li>
-                </ul>
-              </li>
-              <li class="department dep-b">
-                <a href="#"><span>Department B</span></a>
-                <ul class="sections">
-                  <li class="section"><a href="#"><span>Section B1</span></a></li>
-                  <li class="section"><a href="#"><span>Section B2</span></a></li>
-                  <li class="section"><a href="#"><span>Section B3</span></a></li>
-                  <li class="section"><a href="#"><span>Section B4</span></a></li>
-                </ul>
-              </li>
-              <li class="department dep-c">
-                <a href="#"><span>Department C</span></a>
-                <ul class="sections">
-                  <li class="section"><a href="#"><span>Section C1</span></a></li>
-                  <li class="section"><a href="#"><span>Section C2</span></a></li>
-                  <li class="section"><a href="#"><span>Section C3</span></a></li>
-                  <li class="section"><a href="#"><span>Section C4</span></a></li>
-                </ul>
-              </li>
-              <li class="department dep-d">
-                <a href="#"><span>Department D</span></a>
-                <ul class="sections">
-                  <li class="section"><a href="#"><span>Section D1</span></a></li>
-                  <li class="section"><a href="#"><span>Section D2</span></a></li>
-                  <li class="section"><a href="#"><span>Section D3</span></a></li>
-                  <li class="section"><a href="#"><span>Section D4</span></a></li>
-                  <li class="section"><a href="#"><span>Section D5</span></a></li>
-                  <li class="section"><a href="#"><span>Section D6</span></a></li>
-                </ul>
-              </li>
-              <li class="department dep-e">
-                <a href="#"><span>Department E</span></a>
-                <ul class="sections">
-                  <li class="section"><a href="#"><span>Section E1</span></a></li>
-                  <li class="section"><a href="#"><span>Section E2</span></a></li>
-                  <li class="section"><a href="#"><span>Section E3</span></a></li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-        </ul>	
-      </li>
-    </ul>			
-  </figure>
-</div>
+<section>
+	<div class="container">
+		<div class="row justify-content-center">
+      
+      
+			<div class="col-8 col-md-6">
+        <div class="jelly-card">
+				  <div class="card py-3 ">
+            
+            <div class="row">
+            <div class="col-10 col-md-4 col-lg-3 jelly-bloc text-center text-md-start">
+                <img src="Images\PrivateVPN_logo.png" class="card-img-top card-pic slowfloat2s" alt="meduse">
+              </div>
+                <div class="col-12 col-md-8 col-lg-8">
+                <div class="card-body">
+                  <h5 class="card-title">
+                    PrivateVPN
+                  </h5>
+                  <div class="line"></div>
+                  <p class="card-text">
+                   </p>
+                   <i class="fas fa-disease slowfloat3s"></i>
+                </div>
+            </div>
+            </div>
+				</div>
+        </div>
+      </div>
+      
+      
+		</div>
+	</div>
+</section>
 
 <style type="text/css">
+  @import url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap');
 
-
-*{
-    -webkit-box-sizing: border-box;
-	-moz-box-sizing: border-box;
-	box-sizing: border-box;
-	position: relative;
+:root 
+{
+  --family:'Nunito', sans-serif;
+}
+* 
+{
+   -webkit-transition: all .3s ease-out;
+   -moz-transition:  all .3s ease-out;
+   -ms-transition: all .3s ease-out;
+   -o-transition:  all .3s ease-out;
+   transition: all .3s ease-out;
 }
 
-.cf:before,
-.cf:after {
-    content: " "; /* 1 */
-    display: table; /* 2 */
+body
+{
+  font-size:17px;
+  color: #666;
+  text-align:justify;
+  font-family: var(--family);
+  background: rgb(0,159,231);
+  background: linear-gradient(326deg, rgba(147,13,224,1) 0%, rgba(0,159,231,1) 15%, rgba(0,222,223,1) 50%, rgba(156,0,224,1) 100%);
+  background-repeat:no-repeat;
+  background-size: 100% 600%;
+  min-height: 100%;
 }
 
-.cf:after {
-    clear: both;
+section 
+{
+  padding-top:25vw;
 }
 
-/**
- * For IE 6/7 only
- * Include this rule to trigger hasLayout and contain floats.
- */
-.cf {
-    *zoom: 1;
+section::before
+{
+ content: url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill='%23FF0066' d='M41.9,-72.2C51.7,-67,55.1,-50.4,63,-36.5C70.9,-22.5,83.3,-11.3,85.9,1.5C88.5,14.3,81.4,28.6,69.8,36.2C58.2,43.7,42.2,44.6,29.9,52.7C17.5,60.9,8.8,76.3,-2,79.7C-12.7,83.2,-25.5,74.7,-36.9,66C-48.3,57.3,-58.3,48.4,-63.4,37.4C-68.4,26.3,-68.5,13.2,-68.5,0C-68.6,-13.2,-68.6,-26.4,-64.4,-39.1C-60.3,-51.7,-52.1,-63.8,-40.7,-68C-29.2,-72.2,-14.6,-68.5,0.7,-69.7C16.1,-71,32.2,-77.3,41.9,-72.2Z' transform='translate(100 100)' /%3E%3C/svg%3E ");
+  opacity:0.6;
+  position:absolute;
+  left: 60%;
+  width: 40vw;
 }
 
-/* Generic styling */
-
-body{
-	background: #F5EEC9;	
+.card 
+{
+  background: rgba(255,255,255,0.4);
+   border-radius: 10px;
+   border: 1px solid rgba(255,255,255,0.4);
+    position:relative;
+  color:white;
+  z-index:1;
+    backdrop-filter: blur(10px);
+  -webkit-backdrop-filter: blur(10px);
+  box-shadow: 0 10px 25px rgba(0,0,0,0.2);
 }
 
-.content{
-	width: 100%;
-	max-width: 1142px;
-	margin: 0 auto;
-	padding: 0 20px;
+.card-pic 
+{
+  width:12rem;
+background-blend-mode: hard-light; 
+  margin:-20vw auto;
 }
 
-a:focus{
-	outline: 2px dashed #f7f7f7;
-}
-
-@media all and (max-width: 767px){
-	.content{
-		padding: 0 20px;
-	}	
-}
-
-ul{
-	padding: 0;
-	margin: 0;
-	list-style: none;		
-}
-
-ul a{
-	display: block;
-	background: #ccc;
-	border: 4px solid #fff;
-	text-align: center;
-	overflow: hidden;
-	font-size: .7em;
-	text-decoration: none;
-	font-weight: bold;
-	color: #333;
-	height: 70px;
-	margin-bottom: -26px;
-	box-shadow: 4px 4px 9px -4px rgba(0,0,0,0.4);
-	-webkit-transition: all linear .1s;
-	-moz-transition: all linear .1s;
-	transition: all linear .1s;
-}
-
-
-@media all and (max-width: 767px){
-	ul a{
-		font-size: 1em;
-	}
-}
-
-
-ul a span{
-	top: 50%;
-	margin-top: -0.7em;
-	display: block;
-}
-
-/*
- 
- */
-
-.administration > li > a{
-	margin-bottom: 25px;
-}
-
-.director > li > a{
-	width: 50%;
-	margin: 0 auto 0px auto;
-}
-
-.subdirector:after{
-	content: "";
-	display: block;
-	width: 0;
-	height: 130px;
-	background: red;
-	border-left: 4px solid #fff;
-	left: 45.45%;
-	position: relative;
-}
-
-.subdirector,
-.departments{
-	position: absolute;
-	width: 100%;
-}
-
-.subdirector > li:first-child,
-.departments > li:first-child{	
-	width: 18.59894921190893%;
-	height: 64px;
-	margin: 0 auto 92px auto;		
-	padding-top: 25px;
-	border-bottom: 4px solid white;
-	z-index: 1;	
-}
-
-.subdirector > li:first-child{
-	float: right;
-	right: 27.2%;
-	border-left: 4px solid white;
-}
-
-.departments > li:first-child{	
-	float: left;
-	left: 27.2%;
-	border-right: 4px solid white;	
-}
-
-.subdirector > li:first-child a,
-.departments > li:first-child a{
-	width: 100%;
-}
-
-.subdirector > li:first-child a{	
-	left: 25px;
-}
-
-@media all and (max-width: 767px){
-	.subdirector > li:first-child,
-	.departments > li:first-child{
-		width: 40%;	
-	}
-
-	.subdirector > li:first-child{
-		right: 10%;
-		margin-right: 2px;
-	}
-
-	.subdirector:after{
-		left: 49.8%;
-	}
-
-	.departments > li:first-child{
-		left: 10%;
-		margin-left: 2px;
-	}
-}
-
-
-.departments > li:first-child a{
-	right: 25px;
-}
-
-.department:first-child,
-.departments li:nth-child(2){
-	margin-left: 0;
-	clear: left;	
-}
-
-.departments:after{
-	content: "";
-	display: block;
-	position: absolute;
-	width: 81.1%;
-	height: 22px;	
-	border-top: 4px solid #fff;
-	border-right: 4px solid #fff;
-	border-left: 4px solid #fff;
-	margin: 0 auto;
-	top: 130px;
-	left: 9.1%
-}
-
-@media all and (max-width: 767px){
-	.departments:after{
-		border-right: none;
-		left: 0;
-		width: 49.8%;
-	}  
-}
-
-@media all and (min-width: 768px){
-	.department:first-child:before,
-   .department:last-child:before{
-    border:none;
+@media(min-width:991px)
+{
+  section 
+  {
+    padding-top:15vw;
+  }
+  section::before 
+  {
+     left: 50%;
+      width: 30vw;
+  }
+  .card-pic 
+  {
+   position:absolute;
+    left:-6vw;
+    top:-5vw;
+    margin:0; 
   }
 }
 
-.department:before{
-	content: "";
-	display: block;
-	position: absolute;
-	width: 0;
-	height: 22px;
-	border-left: 4px solid white;
-	z-index: 1;
-	top: -22px;
-	left: 50%;
-	margin-left: -4px;
+h5
+{
+  font-size:1.8rem;
+  font-weight:bold;
 }
 
-.department{
-	border-left: 4px solid #fff;
-	width: 18.59894921190893%;
-	float: left;
-	margin-left: 1.751313485113835%;
-	margin-bottom: 60px;
+h5:: 
+{
+ font-family: "Font Awesome 5 Free"; font-weight: 900; content: "\f004";
+  color:#5CF5CB;
 }
 
-.lt-ie8 .department{
-	width: 18.25%;
+.line 
+{
+  width:100%;
+  height:4px;
+  background-color:#0000;
+  opacity:0.7;
+  margin-bottom:10px;
 }
 
-@media all and (max-width: 767px){
-	.department{
-		float: none;
-		width: 100%;
-		margin-left: 0;
-	}
-
-	.department:before{
-		content: "";
-		display: block;
-		position: absolute;
-		width: 0;
-		height: 60px;
-		border-left: 4px solid white;
-		z-index: 1;
-		top: -60px;
-		left: 0%;
-		margin-left: -4px;
-	}
-
-	.department:nth-child(2):before{
-		display: none;
-	}
+i
+{
+  position:absolute;
+  right:-2vw;
+  bottom:-4vw;
+  z-index:2;
+  color:white;
+	font-size:3rem;
+  background: rgba(255,255,255,255);
+  border: 1px solid rgba(255,255,255,0.2);
+	padding: 1rem;
+	border-radius: 10px;
+	transition: all .6s ease-in-out;
+   backdrop-filter: blur(15px);
+  -webkit-backdrop-filter: blur(5px);
+  box-shadow: 0 10px 25px rgba(0,0,0,0.2);
 }
 
-.department > a{
-	margin: 0 0 -26px -4px;
-	z-index: 1;
+@-webkit-keyframes SlowFloat {
+    0% { transform: translateY(0); }
+    100% { transform: translateY(-10px); }
+}
+.slowfloat2s {
+    -webkit-animation: SlowFloat 1s infinite  alternate;
+    animation: SlowFloat 1s infinite  alternate;
 }
 
-.department > a:hover{	
-	height: 80px;
+.slowfloat3s {
+    -webkit-animation: SlowFloat 3s infinite  alternate;
+    animation: SlowFloat 3s infinite  alternate;
 }
-
-.department > ul{
-	margin-top: 0px;
-	margin-bottom: 0px;
-}
-
-.department li{	
-	padding-left: 25px;
-	border-bottom: 4px solid #fff;
-	height: 80px;	
-}
-
-.department li a{
-	background: #fff;
-	top: 48px;	
-	position: absolute;
-	z-index: 1;
-	width: 90%;
-	height: 60px;
-	vertical-align: middle;
-	right: -1px;
-	background-image: url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/Pgo8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgdmlld0JveD0iMCAwIDEgMSIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+CiAgPGxpbmVhckdyYWRpZW50IGlkPSJncmFkLXVjZ2ctZ2VuZXJhdGVkIiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSIgeDE9IjAlIiB5MT0iMCUiIHgyPSIxMDAlIiB5Mj0iMTAwJSI+CiAgICA8c3RvcCBvZmZzZXQ9IjAlIiBzdG9wLWNvbG9yPSIjMDAwMDAwIiBzdG9wLW9wYWNpdHk9IjAuMjUiLz4KICAgIDxzdG9wIG9mZnNldD0iMTAwJSIgc3RvcC1jb2xvcj0iIzAwMDAwMCIgc3RvcC1vcGFjaXR5PSIwIi8+CiAgPC9saW5lYXJHcmFkaWVudD4KICA8cmVjdCB4PSIwIiB5PSIwIiB3aWR0aD0iMSIgaGVpZ2h0PSIxIiBmaWxsPSJ1cmwoI2dyYWQtdWNnZy1nZW5lcmF0ZWQpIiAvPgo8L3N2Zz4=);
-	background-image: -moz-linear-gradient(-45deg,  rgba(0,0,0,0.25) 0%, rgba(0,0,0,0) 100%) !important;
-	background-image: -webkit-gradient(linear, left top, right bottom, color-stop(0%,rgba(0,0,0,0.25)), color-stop(100%,rgba(0,0,0,0)))!important;
-	background-image: -webkit-linear-gradient(-45deg,  rgba(0,0,0,0.25) 0%,rgba(0,0,0,0) 100%)!important;
-	background-image: -o-linear-gradient(-45deg,  rgba(0,0,0,0.25) 0%,rgba(0,0,0,0) 100%)!important;
-	background-image: -ms-linear-gradient(-45deg,  rgba(0,0,0,0.25) 0%,rgba(0,0,0,0) 100%)!important;
-	background-image: linear-gradient(135deg,  rgba(0,0,0,0.25) 0%,rgba(0,0,0,0) 100%)!important;
-	filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#40000000', endColorstr='#00000000',GradientType=1 );
-}
-
-.department li a:hover{
-	box-shadow: 8px 8px 9px -4px rgba(0,0,0,0.1);
-	height: 80px;
-	width: 95%;
-	top: 39px;
-	background-image: none!important;
-}
-
-/* Department/ section colors */
-.department.dep-a a{ background: #FFD600; }
-.department.dep-b a{ background: #AAD4E7; }
-.department.dep-c a{ background: #FDB0FD; }
-.department.dep-d a{ background: #A3A2A2; }
-.department.dep-e a{ background: #f0f0f0; }
 </style>
+
