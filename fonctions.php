19e9adf43cc4e1206a591146057517d25fcecc2a
JeanJaaj <130503436+JeanJaaj@users.noreply.github.com>
2023-06-10 12:01:41 +0200

    Modal AddPartenaire FINITO
    Ajout partenaire dans json + création de photo de profil pour ces derniers

--- fonctions.php
+++ fonctions.php
@@ -958,83 +958,115 @@ function ajout_collab_format()
         }
         file_put_contents('Data\annuaire.json', json_encode($annuaire));
       }
-      function gestion_partenaires(){
- ?>
-    </div>
-    <div class='row mt-5'>
-        <div class="col">
-        </div>
-        <div class="col text-center">
-            <div class='card shadow-sm'>
-                <div class='card-body'>
-                    <h5 class='card-title'>Nouveau Partenaire</h5>
-
-                    <form action='' method='post'>
-                        <div class='form-group'>
-                            <p>Ajouter ou supprimer un partenaire du site web</p>
-                        </div>
-                        <div class="row">
-                            <div class="col">
-                                <button type='button' class='mt-2 btn btn-success' data-bs-toggle="modal" data-bs-target="#addModal">Ajouter</button>
-                            </div>
-                            <div class="col">
-                                <button type='button' class='mt-2 btn btn-danger' data-bs-toggle="modal" data-bs-target="#deleteModal">Supprimer</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-        <div class="col"></div>
-    </div>
-
-    <!-- Add Modal -->
-    <div class="modal fade" id="addModal" tabindex="-1" aria-labelledby="addModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="addModalLabel">Ajouter un partenaire</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form action='' method='post'>
-                        <div class='form-group'>
-                            <input type='text' class='form-control' name='new_partenaire_name' placeholder='Nom du partenaire'><br>
-                            <input type='text' class='form-control' name='new_partenaire_name' placeholder='Description du partenaire'>
-                        </div>
-                        <button type='submit' class='mt-2 btn btn-success'>Ajouter</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Delete Modal -->
-    <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="deleteModalLabel">Supprimer un partenaire</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form action='' method='post'>
-                        <input type='text' class='form-control' name='partenaire_todel' placeholder='Nom du partenaire'><br>
-                        <button type='submit' class='mt-2 btn btn-danger'>Supprimer</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-<?php
+      function gestion_partenaires()
+      {
+          if (isset($_POST['ajouter'])) {
+              $nom = $_POST['new_partenaire_name'];
+              $description = $_POST['new_partenaire_description'];
+              $img = $_FILES['photopart']['name'];
+      
+              if (isset($_FILES['photopart']) && $_FILES['photopart']['error'] === 0) {
+                  $targetDir = 'Images/Partenaires/';
+                  $targetFile = $targetDir . $nom . '.jpg';
+      
+                  if (file_exists($targetFile)) {
+                      unlink($targetFile);
+                  }
+      
+                  $tmpFile = $_FILES['photopart']['tmp_name'];
+                  $newFile = $targetDir . $nom . '.jpg';
+                  move_uploaded_file($tmpFile, $newFile);
+              }
+      
+              if (!partenaire_exists($nom)) {
+                  addPartenaire($nom, $description, $img);
+                  echo "<br><div class='alert alert-success'>Le partenaire a été ajouté avec succès.</div>";
+              } else {
+                  echo "<br><div class='alert alert-danger'>Le partenaire existe déjà.</div>";
+              }
+          } elseif (isset($_POST['supprimer'])) {
+              $nom = $_POST['supprimer'];
+      
+              if (partenaire_exists($nom)) {
+                  delPartenaire($nom);
+                  echo "<br><div class='alert alert-success'>Le partenaire a été supprimé avec succès.</div>";
+              } else {
+                  echo "<br><div class='alert alert-danger'>Le partenaire n'existe pas.</div>";
+              }
+          } else {
+      ?>
+              </div>
+              <div class='row mt-5'>
+                  <div class="col">
+                  </div>
+                  <div class="col text-center">
+                      <div class='card shadow-sm'>
+                          <div class='card-body'>
+                              <h5 class='card-title'>Nouveau Partenaire</h5>
+      
+                              <form action='' method='post'>
+                                  <div class='form-group'>
+                                      <p>Ajouter ou supprimer un partenaire du site web</p>
+                                  </div>
+                                  <div class="row">
+                                      <div class="col">
+                                          <button type='button' class='mt-2 btn btn-success' data-bs-toggle="modal" data-bs-target="#addModal">Ajouter</button>
+                                      </div>
+                                  </div>
+                              </form>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col"></div>
+              </div>
+      
+              <!-- Add Modal -->
+              <div class="modal fade" id="addModal" tabindex="-1" aria-labelledby="addModalLabel" aria-hidden="true">
+                  <div class="modal-dialog">
+                      <div class="modal-content">
+                          <div class="modal-header">
+                              <h5 class="modal-title" id="addModalLabel">Ajouter un partenaire</h5>
+                              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                          </div>
+                          <div class="modal-body">
+                              <form method='post' enctype='multipart/form-data'>
+                                  <div class='form-group'>
+                                      <input type='text' class='form-control' name='new_partenaire_name' placeholder='Nom du partenaire' required><br>
+                                      <input type='text' class='form-control' name='new_partenaire_description' placeholder='Description du partenaire' required>
+                                      <div class="form-group">
+                                          <input type="file" class="form-control-file" id="photopart" name="photopart">
+                                      </div>
+                                  </div>
+                                  <button type='submit' class='mt-2 btn btn-success' name="ajouter">Ajouter</button>
+                              </form>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+      <?php
+          }
+      }
+function partenaire_exists($nom)
+{
+    $partenaires = json_decode(file_get_contents("Data/ListePartenaire.json"), true);
+    return isset($partenaires[$nom]);
 }
 
-        function partenaire_exists(){
+function addPartenaire($nom, $description,$img)
+{
+    $partenaires = json_decode(file_get_contents("Data/ListePartenaire.json"), true);
+    $partenaires[$nom] = array(
+        "description" => $description,
+        "img" => $img
+    );
+    file_put_contents("Data/ListePartenaire.json", json_encode($partenaires));
+}
 
-        }
-        function addPartenaire(){
-            
-        }
-        function delPartenaire(){
-            
-        }
+function delPartenaire($nom)
+{
+    $partenaires = json_decode(file_get_contents("Data/ListePartenaire.json"), true);
+    if (isset($partenaires[$nom])) {
+        unset($partenaires[$nom]);
+        file_put_contents("Data/ListePartenaire.json", json_encode($partenaires));
+    }
+}
