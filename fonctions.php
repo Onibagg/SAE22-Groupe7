ba9fb9d00471d8bf0f8b2ff5dca0c15b32cffc3c
Omega7222 <129857024+Omega7222@users.noreply.github.com>
2023-06-11 10:28:19 +0200

    Affichage partenaires site vitrine

--- fonctions.php
+++ fonctions.php
@@ -1120,6 +1120,34 @@ function ajout_collab_format()
               delPartenaire($nom);
           }
       }
+
+      function display_partenaires_site_vitrine()
+      {
+          $json_file = 'Data/ListePartenaire.json';
+          $partenaires = json_decode(file_get_contents($json_file), true);
+      
+          echo "<div class='row'>"; // Start row
+      
+          foreach ($partenaires as $nom => $partenaire) {
+              $description = $partenaire['description'];
+              $image = $partenaire['partenaire_logo'];
+      
+              echo "
+              <div class='col-md-4'>
+                  <div class='card mb-4'>
+                      <img src='/Images/Partenaires/$image' class='card-img-top' alt='$nom' style='height: 200px; object-fit: contain;'>
+                      <div class='card-body'>
+                          <h5 class='card-title'>$nom</h5>
+                          <p class='card-text'>$description</p>
+                      </div>
+                  </div>
+              </div>";
+          }
+      
+          echo "</div>"; // End row
+      
+        
+      }
       
       
       
