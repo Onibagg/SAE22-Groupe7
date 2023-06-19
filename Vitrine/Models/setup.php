413d2034824dfcef08977d07898c96196199b377
Onibagg <129855477+Onibagg@users.noreply.github.com>
2023-06-20 01:57:52 +0200

    Update setup.php

--- Vitrine/Models/setup.php
+++ Vitrine/Models/setup.php
@@ -7,10 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link href="Style\style.css" type="text/css" rel="stylesheet">
-    <link href="Style\page-histoire-style.css" type="text/css" rel="stylesheet">
-    <link href="Style\intranet-navbar.css" type="text/css" rel="stylesheet">
-    <link href="Style\page-activite-style.css" type="text/css" rel="stylesheet">
+    <link href="./Vitrine/Assets/page_activites.css" type="text/css" rel="stylesheet">
     <link rel="icon" type="image/x-icon" href="../Data/Images/PrivateVPN_logo.png">
     <title>Private VPN | Le meilleur des VPN</title>
     <style>
